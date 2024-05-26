0797d4025350f3077a10abbb7ee6ea061fbc1890
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2024-05-26 20:29:59 +0500

    make concept for project grid

--- apps/web/app/yz13/page.tsx
+++ apps/web/app/yz13/page.tsx
@@ -1,5 +1,9 @@
+import { cn } from "@repo/ui/cn"
 import { getProjects } from "@yz13/api/db/project"
+import { getStorageItem } from "@yz13/supabase/storage"
+import dayjs from "dayjs"
 import Image from "next/image"
+import Link from "next/link"
 import { SidebarLarge } from "./sidebar-large"
 
 const page = async () => {
@@ -8,22 +12,50 @@ const page = async () => {
   return (
     <div className="w-full h-screen flex flex-row">
       <div className="md:block hidden w-full no-scrollbar overflow-y-auto h-full p-3 space-y-3">
-        <div className="grid w-full grid-cols-1 gap-6 lg:grid-cols-3 md:grid-cols-2 auto-rows-auto h-fit">
+        <div className="max-w-xl mx-auto w-full">
           {
-            projects.map(pr =>
-              <div key={pr.id} className="rounded-xl border border-border/20 relative overflow-hidden w-full aspect-[4/3]">
-                {
-                  pr.thumbnail &&
-                  <Image src={pr.thumbnail} fill className="rounded-xl object-cover" alt="project-thumbnail" />
-                }
-                <div className="w-fit absolute bottom-3 right-3 flex flex-col items-end gap-1">
-                  <span className="px-2 py-1 text-xs rounded-md bg-accents-1 w-fit">{pr.name}</span>
+            projects.map(pr => {
+              const time_created_at = dayjs(pr.created_at).format("HH:ss")
+              const date_created_at = dayjs(pr.created_at).format("DD MMMM YYYY")
+              const attachments = pr.attachments.map(attachment => getStorageItem([attachment]))
+              return <section key={pr.id} className="w-full p-6">
+                <div className="flex items-center gap-2 relative">
+                  <Link href={`/yz13/${pr.id}`} className="w-full h-full absolute left-0 top-0" prefetch />
+                  <h2 className="text-4xl font-bold">{pr.name}</h2>
+                  <div className="flex flex-col">
+                    <span className="text-xs">{time_created_at}</span>
+                    <span className="text-xs">{date_created_at}</span>
+                  </div>
+                </div>
+                <div className="w-full py-3">
                   {
                     pr.description &&
-                    <span className="px-2 py-1 text-xs rounded-md bg-accents-1 w-fit">{pr.description}</span>
+                    <p className="font-headings text-2xl">{pr.description}</p>
                   }
                 </div>
-              </div>
+                <div className="flex flex-row items-start gap-1 flex-wrap">
+                  {
+                    pr.tags.map(tag =>
+                      <span key={tag} className="px-2 py-1 rounded-full border text-xs">{tag}</span>
+
+                    )
+                  }
+                </div>
+                <div className="w-full gap-4 attachments-grid grid mt-6">
+                  {
+                    attachments.map(
+                      (attachment, index) =>
+                        <Image
+                          key={attachment}
+                          src={attachment}
+                          className={cn("!relative rounded-xl object-cover", index === 0 ? "attachment-a" : index === 1 ? "attachment-b" : "attachment-c")}
+                          fill alt="attachment"
+                        />
+                    )
+                  }
+                </div>
+              </section>
+            }
             )
           }
         </div>
