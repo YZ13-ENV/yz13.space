22879dfd853c5e291ba323669b16005efda86f39
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2024-06-27 14:25:28 +0500

    made a redesign

--- app/(services)/(routes)/services/page.tsx
+++ app/(services)/(routes)/services/page.tsx
@@ -1,4 +1,5 @@
 import { LeftSide } from "@/app/_components/left"
+import { Nav } from "@/app/_components/nav"
 import { RightSide } from "@/app/_components/right"
 import { SplitViewContainer } from "@/app/_components/split-view-container"
 import { cn } from "@/packages/ui/lib/utils"
@@ -24,58 +25,59 @@ const page = () => {
           </div>
         </div>
       </LeftSide>
-      <RightSide>
-        <>
-          <ul>
-            {
-              registered_services.map(service => {
-                const isSingleRoute = service.routes.length === 1
-                const isExternal = service.isExternal ? service.isExternal : false
-                return (
-                  <li key={service.service_id}>
-                    <section className="w-full border-r border-b p-4 space-y-3">
-                      <div className="space-y-2">
-                        <div className="flex items-center gap-2">
-                          <h2 className="text-2xl font-bold">{service.title}</h2>
-                          {
-                            isExternal &&
-                            <span className="text-xs text-accents-5">(External service)</span>
-                          }
-                        </div>
+      <RightSide className="divide-y">
+        <div className="w-full h-16 flex items-center px-6">
+          <Nav />
+        </div>
+        <ul className="divide-y">
+          {
+            registered_services.map(service => {
+              const isSingleRoute = service.routes.length === 1
+              const isExternal = service.isExternal ? service.isExternal : false
+              return (
+                <li key={service.service_id}>
+                  <section className="w-full border-r p-4 space-y-3">
+                    <div className="space-y-2">
+                      <div className="flex items-center gap-2">
+                        <h2 className="text-2xl font-bold">{service.title}</h2>
                         {
-                          service.description &&
-                          <p className="text-secondary">{service.description}</p>
+                          isExternal &&
+                          <span className="text-xs text-accents-5">(External service)</span>
                         }
                       </div>
-                      <div className={cn(
-                        "w-full grid auto-rows-auto gap-3",
-                        isSingleRoute ? "grid-cols-1" : "md:grid-cols-3 grid-cols-2"
-                      )}>
-                        {
-                          service.routes.map(
-                            route =>
-                              <Link
-                                href={`/service/${service.service_id}${route.route}`}
-                                key={service.service_id + "-" + route.route}
-                                className="w-full h-fit hover:bg-accents-1 hover:border-foreground transition-colors p-3 rounded-lg border flex flex-col gap-2"
-                              >
-                                {
-                                  route.icon
-                                    ? route.icon({ size: 18 })
-                                    : <MdOutlineRoute size={18} />
-                                }
-                                <span className="line-clamp-1 text-sm">{route.title}</span>
-                              </Link>
-                          )
-                        }
-                      </div>
-                    </section>
-                  </li>
-                )
-              })
-            }
-          </ul>
-        </>
+                      {
+                        service.description &&
+                        <p className="text-secondary">{service.description}</p>
+                      }
+                    </div>
+                    <div className={cn(
+                      "w-full grid auto-rows-auto gap-3",
+                      isSingleRoute ? "grid-cols-1" : "md:grid-cols-3 grid-cols-2"
+                    )}>
+                      {
+                        service.routes.map(
+                          route =>
+                            <Link
+                              href={`/service/${service.service_id}${route.route}`}
+                              key={service.service_id + "-" + route.route}
+                              className="w-full h-fit hover:bg-accents-1 hover:border-foreground transition-colors p-3 rounded-lg border flex flex-col gap-2"
+                            >
+                              {
+                                route.icon
+                                  ? route.icon({ size: 18 })
+                                  : <MdOutlineRoute size={18} />
+                              }
+                              <span className="line-clamp-1 text-sm">{route.title}</span>
+                            </Link>
+                        )
+                      }
+                    </div>
+                  </section>
+                </li>
+              )
+            })
+          }
+        </ul>
       </RightSide>
     </SplitViewContainer>
   )
