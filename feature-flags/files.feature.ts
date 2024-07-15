1ce21141fba6ce8d9de362299628a2e6494c9d15
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2024-07-15 10:15:33 +0500

    update decide func in flags

--- feature-flags/files.feature.ts
+++ feature-flags/files.feature.ts
@@ -1,14 +1,15 @@
 import { get } from "@vercel/edge-config";
 import { unstable_flag as flag } from "@vercel/flags/next";
+import { Features } from "./status.feature";
 
 export const showFilesPage = flag<boolean>({
   key: "files-page",
   async decide() {
-    const key = this.key
+    const key = this.key;
     const isDev = process.env.NODE_ENV === "development";
     if (isDev) return isDev;
-    const value = await get<boolean>(key)
-    return value ?? false
+    const features = await get<Features>("features");
+    return features ? features[key] ?? false : false;
   },
   origin: "https://www.yz13.space/flags/files-page/",
   description: "Show status page link in dock",
--- feature-flags/status.feature.ts
+++ feature-flags/status.feature.ts
@@ -1,14 +1,16 @@
 import { get } from "@vercel/edge-config";
 import { unstable_flag as flag } from "@vercel/flags/next";
 
+export type Features = { [key: string]: boolean };
+
 export const showStatus = flag<boolean>({
   key: "status",
   async decide() {
     const key = this.key;
     const isDev = process.env.NODE_ENV === "development";
     if (isDev) return isDev;
-    const value = await get<boolean>(key);
-    return value ?? false;
+    const features = await get<Features>("features");
+    return features ? features[key] ?? false : false;
   },
   origin: "https://www.yz13.space/flags/status/",
   description: "Show status on home page",
