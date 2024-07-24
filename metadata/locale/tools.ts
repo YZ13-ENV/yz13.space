14144f6aba73ce54f86732c7467a12bc16176f17
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2024-07-24 13:26:45 +0500

    added caching to locale metadata

--- metadata/locale/tools.ts
+++ metadata/locale/tools.ts
@@ -1,12 +1,24 @@
+import { unstable_cache as cache } from "next/cache";
 import "server-only";
 
 export type Locales = "en" | "ru";
 export const locales = ["en", "ru"];
 
-const getLocaleMetadata = <T extends any>(
+const getLocaleMetadata = async <T extends any>(
   dict: string,
   locale: Locales
-): Promise<T> =>
-  import(`./${dict}/${locale}.json`).then((module) => module.default);
+): Promise<T> => {
+  const key = `metadata-dict-${locale}`;
+  const getCachedModule = cache(
+    (dict: string, locale: Locales) => import(`./${dict}/${locale}.json`),
+    [key],
+    {
+      revalidate: 60 * 60,
+    }
+  );
+  const module = await getCachedModule(dict, locale);
+  const defaultModule = module;
+  return defaultModule;
+};
 
 export { getLocaleMetadata };
