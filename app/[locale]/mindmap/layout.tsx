0637a083ec6f37b4163a4eb0e6ff924f4afac7d7
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2024-10-05 11:29:07 +0500

    removed mindmap, puzzle, tetris

--- app/[locale]/mindmap/layout.tsx
+++
@@ -1,9 +0,0 @@
-import "@xyflow/react/dist/base.css";
-
-type LayoutProps = Readonly<{
-  children?: React.ReactNode;
-}>;
-const layout = ({ children }: LayoutProps) => {
-  return children
-}
-export default layout
